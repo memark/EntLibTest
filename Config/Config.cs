f4f6f97dc2d08b395c4563886e801a8548f1d802
Magnus Markling <magnus@markling.com>
2019-02-06 07:15:34 +0100

    Initial commit.

---
+++ Config/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace Config
+{
+    public static class Config
+    {
+        public static string connectionString = @"Server=(LocalDB)\ProjectsV13;Database=master";
+
+        public static string StoredProcedureName = "sys.sp_help";
+
+        public static string[] parameterValues = new[] { "abc" };
+    }
+}
---
+++ EntLibNetCore.6.0.1313.DotNetCoreApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+using System;
+
+namespace EntLibNetCore._6._0._1313.DotNetCoreApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var database = new SqlDatabase(Config.Config.connectionString);
+            Console.WriteLine($"DbProviderFactory: {database.DbProviderFactory}");
+
+            var command = database.GetStoredProcCommand(Config.Config.StoredProcedureName, Config.Config.parameterValues);
+            Console.WriteLine($"command: {command}");
+        }
+    }
+}
